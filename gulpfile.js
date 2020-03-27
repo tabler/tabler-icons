97462703ac7c8655ca64c92789b5fb5bd97cff25
codecalm <codecalm@gmail.com>
2020-03-27 21:55:07 +0100

    changelog image fix

--- gulpfile.js
+++ gulpfile.js
@@ -426,15 +426,15 @@ gulp.task('changelog-commit', function (cb) {
 	cp.exec('git status', function (err, ret) {
 		let newIcons = [], modifiedIcons = [], renamedIcons = [];
 
-		ret.replace(/new file:\s+src\/_icons\/([a-z1-9-]+)\.svg/g, function (m, fileName) {
+		ret.replace(/new file:\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function (m, fileName) {
 			newIcons.push(fileName);
 		});
 
-		ret.replace(/modified:\s+src\/_icons\/([a-z1-9-]+)\.svg/g, function (m, fileName) {
+		ret.replace(/modified:\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function (m, fileName) {
 			modifiedIcons.push(fileName);
 		});
 
-		ret.replace(/renamed:\s+src\/_icons\/([a-z1-9-]+).svg -> src\/_icons\/([a-z1-9-]+).svg/g, function (m, fileNameBefore, fileNameAfter) {
+		ret.replace(/renamed:\s+src\/_icons\/([a-z0-9-]+).svg -> src\/_icons\/([a-z0-9-]+).svg/g, function (m, fileNameBefore, fileNameAfter) {
 			renamedIcons.push([fileNameBefore, fileNameAfter]);
 		});
 
@@ -456,15 +456,15 @@ gulp.task('changelog', function (cb) {
 
 			let newIcons = [], modifiedIcons = [], renamedIcons = [];
 
-			ret.replace(/A\s+src\/_icons\/([a-z1-9-]+)\.svg/g, function (m, fileName) {
+			ret.replace(/A\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function (m, fileName) {
 				newIcons.push(fileName);
 			});
 
-			ret.replace(/M\s+src\/_icons\/([a-z1-9-]+)\.svg/g, function (m, fileName) {
+			ret.replace(/M\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function (m, fileName) {
 				modifiedIcons.push(fileName);
 			});
 
-			ret.replace(/R[0-9]+\s+src\/_icons\/([a-z1-9-]+)\.svg\s+src\/_icons\/([a-z1-9-]+).svg/g, function (m, fileNameBefore, fileNameAfter) {
+			ret.replace(/R[0-9]+\s+src\/_icons\/([a-z0-9-]+)\.svg\s+src\/_icons\/([a-z0-9-]+).svg/g, function (m, fileNameBefore, fileNameAfter) {
 				renamedIcons.push([fileNameBefore, fileNameAfter]);
 			});
 
