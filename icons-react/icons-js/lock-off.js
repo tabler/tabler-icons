27e9587463368936da664db9abeac4bcbc20136d
codecalm <codecalm@gmail.com>
2020-09-22 13:58:51 +0200

    Release 1.25.0

---
+++ icons-react/icons-js/lock-off.js
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const IconLockOff = (size = 24, color = "currentColor", stroke = 2, ...props) => <svg className="icon icon-tabler icon-tabler-lock-off" width={size} height={size} viewBox="0 0 24 24" strokeWidth={stroke} stroke={color} fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><line x1={3} y1={3} x2={21} y2={21} /><path d="M19 19a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2h4m4 0h2a2 2 0 0 1 2 2v2" /><circle cx={12} cy={16} r={1} /><path d="M8 11v-3m.712 -3.278a4 4 0 0 1 7.288 2.278v4" /></svg>;
+
+export default IconLockOff;
