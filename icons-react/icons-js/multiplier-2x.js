8513dcac6c76e3d8cf08b41ab12c1b24df8bf29e
codecalm <codecalm@gmail.com>
2021-11-03 22:01:48 +0100

    Release 1.42.0

---
+++ icons-react/icons-js/multiplier-2x.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function IconMultiplier2x({
+  size = 24,
+  color = "currentColor",
+  stroke = 2,
+  ...props
+}) {
+  return <svg className="icon icon-tabler icon-tabler-multiplier-2x" width={size} height={size} viewBox="0 0 24 24" strokeWidth={stroke} stroke={color} fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 16l4 -4" /><path d="M18 16l-4 -4" /><path d="M6 10a2 2 0 1 1 4 0c0 .591 -.417 1.318 -.816 1.858l-3.184 4.143l4 -.001" /></svg>;
+}
+
+export default IconMultiplier2x;
