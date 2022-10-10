883f08880357e4a2cf4bb8ce4bdbad85b41a65e4
codecalm <codecalm@gmail.com>
2022-10-10 20:53:43 +0200

    update tags

--- gulpfile.js
+++ gulpfile.js
@@ -774,16 +774,24 @@ gulp.task('import-categories', (cb) => {
     if (fs.existsSync(fileOriginal)) {
       const dataOriginal = fs.readFileSync(fileOriginal).toString()
 
-      const categoryOriginal = dataOriginal.match(/category: ([a-zA-Z-]+)/)
+      const categoryOriginal = dataOriginal.match(/category: ([a-zA-Z-]+)/),
+          tagsOriginal = dataOriginal.match(/tags: (\[.*?\])/)
 
-      if (categoryOriginal) {
-        console.log('categoryOriginal', categoryOriginal[1])
+      if (categoryOriginal || tagsOriginal) {
 
         let data = fs.readFileSync(file).toString()
         data = data.replace(/(---[\s\S]+?---)/, function(m, headerContent) {
+          console.log('categoryOriginal', fileOriginal, categoryOriginal && categoryOriginal[1], tagsOriginal && tagsOriginal[1])
 
-          headerContent = headerContent.replace(/category: .*\n/, '')
-          headerContent = headerContent.replace(/---/, `---\ncategory: ${categoryOriginal[1]}`)
+          if (categoryOriginal) {
+            headerContent = headerContent.replace(/category: .*\n/, '')
+            headerContent = headerContent.replace(/---/, `---\ncategory: ${categoryOriginal[1]}`)
+          }
+
+          if (tagsOriginal) {
+            headerContent = headerContent.replace(/tags: .*\n/, '')
+            headerContent = headerContent.replace(/---/, `---\ntags: ${tagsOriginal[1]}`)
+          }
 
           return headerContent
         })
