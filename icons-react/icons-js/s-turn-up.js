c0ad3e10eff6343a55d69eeba893948bcb5b9068
codecalm <codecalm@gmail.com>
2022-10-31 12:27:21 +0100

    Release 1.109.0

--- icons-react/icons-js/s-turn-up.js
+++ icons-react/icons-js/s-turn-up.js
@@ -6,7 +6,7 @@ function IconSTurnUp({
   stroke = 2,
   ...props
 }) {
-  return <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-s-turn-up" width={size} height={size} viewBox="0 0 24 24" strokeWidth={stroke} stroke={color} fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6.5 19.5a2 2 0 1 0 -4 0a2 2 0 0 0 4 0z" /><path d="M4.5 17.5v-9.5a3.5 3.5 0 0 1 7 0v9a3.5 3.5 0 0 0 7 0v-13.5" /><path d="M15.5 6.5l3 -3l3 3" /></svg>;
+  return <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-s-turn-up" width={size} height={size} viewBox="0 0 24 24" strokeWidth={stroke} stroke={color} fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M7 19a2 2 0 1 0 -4 0a2 2 0 0 0 4 0z" /><path d="M5 17v-9.5a3.5 3.5 0 0 1 7 0v9a3.5 3.5 0 0 0 7 0v-13.5" /><path d="M16 6l3 -3l3 3" /></svg>;
 }
 
 export default IconSTurnUp;
