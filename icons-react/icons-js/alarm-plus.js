b6adde29e8127d194e0b9a26b936b5548a1b6f6f
codecalm <codecalm@gmail.com>
2022-12-13 19:59:44 +0100

    Release 1.117.0

---
+++ icons-react/icons-js/alarm-plus.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function IconAlarmPlus({
+  size = 24,
+  color = "currentColor",
+  stroke = 2,
+  ...props
+}) {
+  return <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-alarm-plus" width={size} height={size} viewBox="0 0 24 24" strokeWidth={stroke} stroke={color} fill="none" strokeLinecap="round" strokeLinejoin="round" {...props}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><circle cx={12} cy={13} r={7} /><path d="M7 4l-2.75 2" /><path d="M17 4l2.75 2" /><path d="M10 13h4" /><path d="M12 11v4" /></svg>;
+}
+
+export default IconAlarmPlus;
