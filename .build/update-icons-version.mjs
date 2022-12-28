c76c2a24731617a871198dbbf797efe2c7f09a20
codecalm <codecalm@gmail.com>
2022-12-28 02:19:53 +0100

    move build scripts from gulp to directory

---
+++ .build/update-icons-version.mjs
@@ -0,0 +1,48 @@
+import p from '../package.json' assert { type: "json" }
+import minimist from 'minimist'
+import cp from 'child_process'
+import fs from 'fs'
+import { join } from 'path'
+import { ICONS_SRC_DIR } from './helpers.mjs'
+
+const argv = minimist(process.argv.slice(2))
+const version = argv['latest-version'] || `${p.version}`,
+    newVersion = argv['new-version'] || `${p.version}`
+
+const setVersions = function(version, files) {
+  for (const i in files) {
+    const file = files[i],
+        filePath = join(ICONS_SRC_DIR, `${file}.svg`)
+
+    if (fs.existsSync(filePath)) {
+      let svgFile = fs.readFileSync(filePath).toString()
+
+      if (!svgFile.match(/version: ([0-9.]+)/i)) {
+        svgFile = svgFile.replace(/---\n<svg>/i, function(m) {
+          return `version: "${version}"\n${m}`
+        })
+
+        fs.writeFileSync(filePath, svgFile)
+      } else {
+        console.log(`File ${file} already has version`)
+      }
+
+    } else {
+      console.log(`File ${file} doesn't exists`)
+    }
+  }
+}
+
+if (version) {
+  cp.exec(`grep -RiL "version: " ${ICONS_SRC_DIR}/*.svg`, function(err, ret) {
+    let newIcons = []
+
+    ret.replace(/src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
+      newIcons.push(fileName)
+    })
+
+    if (newIcons.length) {
+      setVersions(newVersion.replace(/\.0$/, ''), newIcons)
+    }
+  })
+}
--- gulpfile.js
+++ gulpfile.js
@@ -334,55 +334,7 @@ gulp.task('iconfont-css', (cb) => {
   })
 })
 
-const getMaxUnicode = () => {
-  const path = 'src/_icons/*.svg'
-  const files = glob.sync(path)
-  let maxUnicode = 0
 
-  files.forEach(function(file) {
-    const svgFile = fs.readFileSync(file).toString()
-
-    svgFile.replace(/unicode: "([a-f0-9.]+)"/i, function(m, unicode) {
-      const newUnicode = parseInt(unicode, 16)
-
-      if (newUnicode) {
-        maxUnicode = Math.max(maxUnicode, newUnicode)
-      }
-    })
-  })
-
-  return maxUnicode
-}
-
-gulp.task('update-icons-unicode', (cb) => {
-  let maxUnicode = getMaxUnicode()
-
-  glob('./src/_icons/*.svg', {}, function(er, files) {
-    for (const i in files) {
-      const file = files[i]
-
-      let svgFile = fs.readFileSync(file).toString()
-
-      if (!svgFile.match(/\nunicode: "?([a-f0-9.]+)"?/i)) {
-        maxUnicode++
-        const unicode = maxUnicode.toString(16)
-
-        if (unicode) {
-          svgFile = svgFile.replace(/---\n<svg>/i, function(m) {
-            return `unicode: "${unicode}"\n${m}`
-          })
-
-          console.log(`Add unicode "${unicode}" to "${file}"`)
-          fs.writeFileSync(file, svgFile)
-        }
-      } else {
-        console.log(`File ${file} already has unicode`)
-      }
-    }
-
-    cb()
-  })
-})
 
 gulp.task('build-iconfont',
     gulp.series('iconfont-prepare', 'iconfont-svg-outline', 'iconfont-fix-outline', 'iconfont-optimize', 'iconfont', 'iconfont-css', 'iconfont-clean'))
@@ -649,48 +601,11 @@ gulp.task('svg-to-png', gulp.series('clean-png', async (cb) => {
 //   cb()
 // }))
 
-const setVersions = function(version, files) {
-  for (const i in files) {
-    const file = files[i]
 
-    if (fs.existsSync(`src/_icons/${file}.svg`)) {
-      let svgFile = fs.readFileSync(`src/_icons/${file}.svg`).toString()
-
-      if (!svgFile.match(/version: ([0-9.]+)/i)) {
-        svgFile = svgFile.replace(/---\n<svg>/i, function(m) {
-          return `version: "${version}"\n${m}`
-        })
-
-        fs.writeFileSync(`src/_icons/${file}.svg`, svgFile)
-      } else {
-        console.log(`File ${file} already has version`)
-      }
-
-    } else {
-      console.log(`File ${file} doesn't exists`)
-    }
-  }
-}
 
 gulp.task('update-icons-version', (cb) => {
 
-  const version = argv['latest-version'] || `${p.version}`,
-      newVersion = argv['new-version'] || `${p.version}`
 
-  if (version) {
-    cp.exec(`grep -RiL "version: " ./src/_icons/*.svg`, function(err, ret) {
-
-      let newIcons = []
-
-      ret.replace(/src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
-        newIcons.push(fileName)
-      })
-
-      if (newIcons.length) {
-        setVersions(newVersion.replace(/\.0$/, ''), newIcons)
-      }
-    })
-  }
 
   cb()
 })
@@ -714,7 +629,7 @@ gulp.task('update-readme', (cb) => {
 // })
 
 gulp.task('build',
-    gulp.series('optimize', 'update-icons-version', 'update-icons-unicode', 'build-jekyll', 'build-copy', 'icons-sprite', /*'svg-to-react', 'build-react',*/
+    gulp.series(/*'optsetVersionsimize', 'update-icons-version', 'update-icons-unicode', 'build-jekyll',*/ 'build-copy', 'icons-sprite', /*'svg-to-react', 'build-react',*/
         'icons-preview', 'svg-to-png', 'build-iconfont', 'changelog-image', 'build-zip', 'update-readme'))
 
 const optimizeSVG = (data) => {
