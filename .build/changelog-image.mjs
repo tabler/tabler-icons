c3b6f828862733bc5d409fb00d8fa639fe2d8ef3
codecalm <codecalm@gmail.com>
2023-01-23 19:15:39 +0100

    changelog image fix

--- .build/changelog-image.mjs
+++ .build/changelog-image.mjs
@@ -1,30 +1,27 @@
-import cp from 'child_process'
-import { generateIconsPreview, getArgvs, getPackageJson } from './helpers.mjs'
+import { generateIconsPreview, getArgvs, getPackageJson, HOME_DIR } from './helpers.mjs'
+import * as fs from 'fs'
 
 const argv = getArgvs(),
     p = getPackageJson()
 
-const version = argv['latest-version'] || `v${p.version}`,
-    newVersion = argv['new-version'] || `${p.version}`
+const version = argv['new-version'] || `${p.version}`
 
 if (version) {
-  cp.exec(`git diff ${version} HEAD --name-status --diff-filter=A src/_icons`, function(err, ret) {
+  const icons = JSON.parse(fs.readFileSync(`${HOME_DIR}/tags.json`))
 
-    let newIcons = []
-
-    ret.replace(/[A]\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
-      newIcons.push(fileName)
-    })
-
-    newIcons = newIcons.map(function(icon) {
-      return `./icons/${icon}.svg`
-    })
-
-    if (newIcons.length > 0) {
-      generateIconsPreview(newIcons, `.github/tabler-icons-${newVersion}.svg`, {
-        columnsCount: 6,
-        paddingOuter: 24
+  const newIcons = Object
+      .entries(icons)
+      .filter(([name, value]) => {
+        return `${value.version}.0` === version
       })
-    }
-  })
+      .map(([name, value]) => {
+        return `./icons/${name}.svg`
+      })
+
+  if (newIcons.length > 0) {
+    generateIconsPreview(newIcons, `.github/tabler-icons-${version}.svg`, {
+      columnsCount: 6,
+      paddingOuter: 24
+    })
+  }
 }
