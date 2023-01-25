ad8cbcc75abfd627d71f3c679dc628f9982372db
codecalm <codecalm@gmail.com>
2023-01-25 23:29:53 +0100

    check-unicodes script

--- .build/check-icons-unicodes.mjs
+++ .build/check-icons-unicodes.mjs
@@ -15,8 +15,6 @@ glob(path.join(ICONS_SRC_DIR, '*.svg'), {}, function(er, files) {
     if (matches[1]) {
       const unicode = matches[1]
 
-      console.log('unicode', unicode, unicodes.indexOf(unicode));
-
       if (unicode && unicodes.indexOf(unicode) === -1) {
         unicodes.push(unicode)
       } else {
@@ -26,9 +24,6 @@ glob(path.join(ICONS_SRC_DIR, '*.svg'), {}, function(er, files) {
       throw new Error(`Icon ${file} don't have a unicode!`)
     }
   }
-
-
-  console.log('unicodes', unicodes);
 })
 
 
