4f7524d936e1506ce0c8b1a3dfc434dea51841cb
codecalm <codecalm@gmail.com>
2023-04-02 16:40:27 +0200

    add missing categories to icons

--- .build/import-categories.mjs
+++ .build/import-categories.mjs
@@ -3,36 +3,40 @@ import fs from 'fs'
 import { resolve, join, basename } from 'path'
 import { ICONS_SRC_DIR } from './helpers.mjs'
 
+const extensions = ['heart', 'star', 'off', 'bolt', 'cancel', 'check', 'cog', 'dollar', 'dot', 'eco', 'edit', 'x', 'plus', 'minus', 'shield', 'up', 'down', 'move', 'link', 'ribbon', 'question', 'exclamation', '2', '3', 'code', 'pause', 'pin', 'search', 'share']
 
-glob.sync(join(ICONS_SRC_DIR, '*-filled.svg')).forEach(function(file, i) {
-  const fileOriginal = file.replace(/\-filled.svg$/, '.svg')
+extensions.forEach(function (extension) {
 
-  if (fs.existsSync(fileOriginal)) {
-    const dataOriginal = fs.readFileSync(fileOriginal).toString()
+  glob.sync(join(ICONS_SRC_DIR, `*-${extension}.svg`)).forEach(function (file, i) {
+    const fileOriginal = file.replace(`-${extension}.svg`, '.svg')
 
-    const categoryOriginal = dataOriginal.match(/category: ([a-zA-Z-]+)/),
+    if (fs.existsSync(fileOriginal)) {
+      const dataOriginal = fs.readFileSync(fileOriginal).toString()
+
+      const categoryOriginal = dataOriginal.match(/category: ([a-zA-Z-]+)/),
         tagsOriginal = dataOriginal.match(/tags: (\[.*?\])/)
 
-    if (categoryOriginal || tagsOriginal) {
+      if (categoryOriginal || tagsOriginal) {
 
-      let data = fs.readFileSync(file).toString()
-      data = data.replace(/(---[\s\S]+?---)/, function(m, headerContent) {
-        console.log('categoryOriginal', fileOriginal, categoryOriginal && categoryOriginal[1], tagsOriginal && tagsOriginal[1])
+        let data = fs.readFileSync(file).toString()
+        data = data.replace(/(---[\s\S]+?---)/, function (m, headerContent) {
+          console.log('categoryOriginal', fileOriginal, categoryOriginal && categoryOriginal[1], tagsOriginal && tagsOriginal[1])
 
-        if (categoryOriginal) {
-          headerContent = headerContent.replace(/category: .*\n/, '')
-          headerContent = headerContent.replace(/---/, `---\ncategory: ${categoryOriginal[1]}`)
-        }
+          if (categoryOriginal) {
+            headerContent = headerContent.replace(/category: .*\n/, '')
+            headerContent = headerContent.replace(/---/, `---\ncategory: ${categoryOriginal[1]}`)
+          }
 
-        if (tagsOriginal) {
-          headerContent = headerContent.replace(/tags: .*\n/, '')
-          headerContent = headerContent.replace(/---/, `---\ntags: ${tagsOriginal[1]}`)
-        }
+          // if (tagsOriginal) {
+          //   headerContent = headerContent.replace(/tags: .*\n/, '')
+          //   headerContent = headerContent.replace(/---/, `---\ntags: ${tagsOriginal[1]}`)
+          // }
 
-        return headerContent
-      })
+          return headerContent
+        })
 
-      fs.writeFileSync(file, data)
+        fs.writeFileSync(file, data)
+      }
     }
-  }
+  })
 })
