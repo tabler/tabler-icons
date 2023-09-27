22d70b2db097915c81c22713bea3331a62def2eb
codecalm <codecalm@gmail.com>
2023-09-27 22:18:52 +0200

    update icons tags

--- .build/import-tags.mjs
+++ .build/import-tags.mjs
@@ -8,19 +8,24 @@ const data = JSON.parse(fs.readFileSync(join(HOME_DIR, 'new/tags.json'), 'utf-8'
 
 for (const [key, tags] of Object.entries(data)) {
 
-  const filename = join(HOME_DIR, `src/_icons/${key}.svg`)
+  const filename = join(HOME_DIR, `src/_icons/${key}.svg`), tagsInline = tags
+    .join(' ')
+    .toLowerCase()
+    .split(' ')
+    .filter((value, index, array) => array.indexOf(value) === index)
+    .join(', ')
 
   if (key && tags.length) {
     let data = fs.readFileSync(filename).toString()
-    data = data.replace(/(---[\s\S]+?---)/, function(m, headerContent) {
+    data = data.replace(/(---[\s\S]+?---)/, function (m, headerContent) {
 
       headerContent = headerContent.replace(/tags: .*\n/, '')
-      headerContent = headerContent.replace(/---/, `---\ntags: [${tags.join(', ')}]`)
+      headerContent = headerContent.replace(/---/, `---\ntags: [${tagsInline}]`)
 
       return headerContent
     })
 
-    console.log(`Updating ${key} with tags: ${tags.join(', ')}`)
+    console.log(`Updating ${key} with tags: ${tagsInline}`)
     fs.writeFileSync(filename, data)
   }
 }
