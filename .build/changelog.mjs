e4db65248b85f136cf6a0597ad18c4357aa37f4c
codecalm <codecalm@gmail.com>
2024-02-17 20:58:26 +0100

    changelog update

--- .build/changelog.mjs
+++ .build/changelog.mjs
@@ -6,19 +6,19 @@ const p = getPackageJson(),
     version = argv['latest-version'] || `${p.version}`
 
 if (version) {
-  cp.exec(`git diff ${version} HEAD --name-status src/_icons`, function(err, ret) {
+  cp.exec(`git diff ${version} HEAD --name-status ./icons`, function(err, ret) {
 
     let newIcons = [], modifiedIcons = [], renamedIcons = []
 
-    ret.replace(/A\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
+    ret.replace(/A\s+icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
       newIcons.push(fileName)
     })
 
-    ret.replace(/M\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
+    ret.replace(/M\s+icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
       modifiedIcons.push(fileName)
     })
 
-    ret.replace(/R[0-9]+\s+src\/_icons\/([a-z0-9-]+)\.svg\s+src\/_icons\/([a-z0-9-]+).svg/g, function(m, fileNameBefore, fileNameAfter) {
+    ret.replace(/R[0-9]+\s+icons\/([a-z0-9-]+)\.svg\s+icons\/([a-z0-9-]+).svg/g, function(m, fileNameBefore, fileNameAfter) {
       renamedIcons.push([fileNameBefore, fileNameAfter])
     })
 
