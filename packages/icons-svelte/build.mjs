3f4512c1642aed080716f330221eccbc63d4c183
codecalm <codecalm@gmail.com>
2024-03-12 01:54:03 +0100

    svelte build improvements

--- packages/icons-svelte/build.mjs
+++ packages/icons-svelte/build.mjs
@@ -2,17 +2,32 @@
 
 import { buildJsIcons } from '../../.build/build-icons.mjs'
 import { readFileSync, writeFileSync } from 'fs'
+import { getAllIcons, toPascalCase } from '../../.build/helpers.mjs'
 
 const updatePackageJson = () => {
   let packageJson = JSON.parse(readFileSync('package.json', 'utf-8'))
 
   if (packageJson.exports) {
-    for(const key in packageJson.exports) {
+    for (const key in packageJson.exports) {
       if (key.match(/^\.\/[^\.]+\.svelte$/)) {
         delete packageJson.exports[key]
       }
     }
   }
+
+  const allIcons = getAllIcons(false, false)
+
+  Object.entries(allIcons).forEach(([type, icons]) => {
+    icons.forEach((icon, i) => {
+      const iconName = `${icon.name}${type !== 'outline' ? `-${type}` : ''}`,
+        iconNamePascal = `${icon.namePascal}${type !== 'outline' ? toPascalCase(type) : ''}`
+
+      packageJson.exports[`./${iconNamePascal}.svelte`] = {
+        types: `./dist/icons/${iconName}.svelte.d.ts`,
+        svelte: `./dist/icons/${iconName}.svelte`
+      }
+    })
+  })
 
   writeFileSync('package.json', JSON.stringify(packageJson, null, 2))
 }
@@ -45,15 +60,15 @@ const indexItemTemplate = ({
   namePascal
 }) => `export { default as ${namePascal} } from './${name}.svelte';`
 
-// buildJsIcons({
-//   name: 'icons-svelte',
-//   componentTemplate,
-//   indexItemTemplate,
-//   aliasTemplate,
-//   extension: 'svelte',
-//   key: false,
-//   indexFile: 'index.ts',
-//   pascalName: false,
-// })
+buildJsIcons({
+  name: 'icons-svelte',
+  componentTemplate,
+  indexItemTemplate,
+  aliasTemplate,
+  extension: 'svelte',
+  key: false,
+  indexFile: 'index.ts',
+  pascalName: false,
+})
 
 updatePackageJson()
