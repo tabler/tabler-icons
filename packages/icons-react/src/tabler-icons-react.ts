08d14a19f87463fedf43acad28d626eafbfcd026
Patrick Naughton <patrickjnaughton@gmail.com>
2024-03-14 11:33:16 -0700

    add TablerIcon type

--- packages/icons-react/src/tabler-icons-react.ts
+++ packages/icons-react/src/tabler-icons-react.ts
@@ -4,4 +4,4 @@ export * as iconsList from './icons-list';
 export * from './aliases';
 export { default as createReactComponent } from './createReactComponent';
 
-export type { Icon, IconNode, IconProps } from './types';
+export type { Icon, IconNode, IconProps, TablerIcon } from './types';
