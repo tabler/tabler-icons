7efd83d2de8fe7c7dcf3db9ad58055f70abea0f7
codecalm <codecalm@gmail.com>
2024-08-26 13:56:42 +0200

    18 new icons: `outline/adjustments-spark`, `outline/battery-spark`, `outline/camera-spark`, `outline/cube-spark`, `outline/eye-spark`, `outline/file-spark`, `outline/file-text-spark`, `outline/flag-spark`, `outline/heart-spark`, `outline/home-spark`, `outline/input-spark`, `outline/mail-spark`, `outline/mood-spark`, `outline/phone-spark`, `outline/photo-spark`, `outline/server-spark`, `outline/settings-spark`, `outline/table-spark`

--- .build/import-categories.mjs
+++ .build/import-categories.mjs
@@ -4,7 +4,7 @@ import { resolve, join, basename } from 'path'
 import { ICONS_SRC_DIR } from './helpers.mjs'
 
 // const extensions = ['heart', 'star', 'off', 'bolt', 'cancel', 'check', 'cog', 'dollar', 'dot', 'eco', 'edit', 'x', 'plus', 'minus', 'shield', 'up', 'down', 'move', 'link', 'ribbon', 'question', 'exclamation', '2', '3', 'code', 'pause', 'pin', 'search', 'share', 'hand', 'infinity', 'ai']
-const extensions = ['off']
+const extensions = ['off', 'spark']
 
 extensions.forEach(function (extension) {
 
