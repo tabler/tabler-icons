855eb28c336d78c7bd95879441069ae459b71495
codecalm <codecalm@gmail.com>
2025-07-20 20:29:37 +0200

    Fix export name for Preact component in icons-preact module

--- packages/icons-preact/src/tabler-icons-preact.ts
+++ packages/icons-preact/src/tabler-icons-preact.ts
@@ -3,6 +3,6 @@ export * as icons from './icons/index';
 export * as iconsList from './icons-list';
 export * from './aliases';
 
-export { default as createReactComponent } from './createPreactComponent';
+export { default as createPeactComponent } from './createPreactComponent';
 
 export type { Icon, IconNode, IconProps } from './types';
