c6e1cbc141fdcc875a13fa80efca938c0943dd16
theetherGit <meenashivam9650@gmail.com>
2025-10-19 14:29:01 +0530

    export types and Icon types

--- packages/icons-svelte-v5/src/tabler-icons-svelte.ts
+++ packages/icons-svelte-v5/src/tabler-icons-svelte.ts
@@ -1,5 +1,7 @@
 export * from "./icons/index.js";
 export * as icons from "./icons/index.js";
-export * as iconsList from './icons-list';
+export * as iconsList from './icons-list.js';
 export * from "./aliases.js";
-export { default as defaultAttributes } from './defaultAttributes'
+export { default as defaultAttributes } from './defaultAttributes.js'
+export * from "./types.js";
+export { default as Icon } from "./Icon.svelte";
