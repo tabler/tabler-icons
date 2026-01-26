88b95f179a29ad1cafb1b492c428cb3cbea67baa
Bartłomiej Gawęda <bagaweda@gmail.com>
2026-01-26 21:38:27 +0100

    Add more samples in test-angular project

--- test/test-angular/src/app/app.component.ts
+++ test/test-angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { TablerIconComponent, IconAd, IconAdOff, IconAdFilled, IconHome, IconHomeOff, IconHomeFilled } from '@tabler/icons-angular';
+import { TablerIconComponent, IconAd, IconAdOff, IconAdFilled, IconHome, IconHomeOff, IconHomeFilled, TablerIcon } from '@tabler/icons-angular';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +16,18 @@ export class AppComponent {
   iconHome = IconHome;
   iconHomeOff = IconHomeOff;
   iconHomeFilled = IconHomeFilled;
+
+  toggledIcon = signal<TablerIcon>(IconHome);
+  defaultSize = signal(48);
+  stroke = signal(1);
+
+  toggleIcon() {
+    this.toggledIcon.update(value => value === IconHome ? IconHomeOff : IconHome);
+  }
+
+  toggleStroke() {
+    this.stroke.update(value => value === 2.5 ? 1 : value + 0.25);
+  }
 
   toggleActive() {
     this.active.update(value => !value);
