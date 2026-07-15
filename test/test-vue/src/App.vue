4a984bc600e2f50f5d7912e41871a58822745ad4
codecalm <codecalm@gmail.com>
2026-07-16 00:54:52 +0200

    Add color toggle functionality and new icons in various frameworks

--- test/test-vue/src/App.vue
+++ test/test-vue/src/App.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
 import { ref } from "vue"
-import { IconAd, IconAdOff, IconAdFilled } from '@tabler/icons-vue'
+import { IconAd, IconAdOff, IconAdFilled, IconHeartFilled } from '@tabler/icons-vue'
 
 const active = ref(false)
+
+const colors = ['#e64980', '#4dabf7', '#51cf66', '#ffd43b', '#845ef7']
+const colorIndex = ref(0)
 </script>
 
 <template>
-  <a @click="active = !active">
-    <IconAdOff v-if="active" size="48" />
-    <IconAd v-else size="48" />
-  </a>
-  <IconAd size="48" stroke="1" />
-  <IconAdOff size="48" stroke="1.5" />
-  <IconAdFilled size="48" stroke="2" />
+  <div class="App">
+    <button type="button" class="toggle" @click="active = !active">
+      <IconAdOff v-if="active" size="48" />
+      <IconAd v-else size="48" />
+    </button>
+    <IconAd size="48" stroke="1" />
+    <IconAdOff size="48" stroke="1.5" />
+    <IconAdFilled size="48" stroke="2" />
+    <button type="button" class="color-toggle" @click="colorIndex = (colorIndex + 1) % colors.length">
+      <IconHeartFilled size="48" :color="colors[colorIndex]" />
+    </button>
+  </div>
 </template>
 
