f96bbd93739953bb8a12b94f4c257298f4f7db84
codecalm <codecalm@gmail.com>
2026-07-19 21:47:40 +0200

    Update .gitignore to include .pnpm-store and modify Angular build configuration by replacing @angular-devkit/build-angular with @angular/build in relevant files for improved compatibility.

--- packages/icons-angular/karma.conf.js
+++ packages/icons-angular/karma.conf.js
@@ -4,13 +4,12 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       jasmine: {
